3b07f1938659db895af822b870113f06d47e5582
1998-max <adiyolo.harelboyenge@student.greenriver.edu>
2025-10-04 21:10:58 -0700

    Added in hello_world.py

--- start/CH01/Hello_World.py
+++ start/CH01/Hello_World.py
@@ -1,1 +1,15 @@
-print("Hello world")
+# Prompts the user for their name
+
+name = input("What is your name?: ")
+print("Hello", name)
+
+# Prints a positive affirmation (such as "Today is going to be a great day!")
+
+print("Today is going to be a great day!")
+
+
+# Prompt the user for their age
+age = input("What is your age?: ")
+number_of_age_in_2_year = int(age) + 2
+
+print("In 2 years, you will be",number_of_age_in_2_year,"years old")
