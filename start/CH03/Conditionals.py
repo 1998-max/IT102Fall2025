3d5a8b0a3fddfe58a390afe2e306f4fc75848507
1998-max <adiyolo.harelboyenge@student.greenriver.edu>
2025-10-11 17:12:16 -0700

    Added Conditionals to Chapter 3

--- start/CH03/Conditionals.py
+++ start/CH03/Conditionals.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
-# example workign with conditionals
-#By 
+# Example working with conditionals
+# By Dillon
+
+# First we have to gather if the day is good or bad, which requires user input
+question = input("Is today a good day? Please answer with y or n: ").lower()
+
+# Analyze the value and print if yes it is
+if question == 'y':
+    print("Yes it is")
+elif question == 'n':
+    print("That's okay, tomorrow will be")
+elif question == 'idk':
+    print("That's okay")
+else:
+    print("Please enter a valid input of y or n for yes or no")
