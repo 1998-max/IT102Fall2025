c7c9d7753c53cbf7b5c631252c2015265dad1efb
1998-max <adiyolo.harelboyenge@student.greenriver.edu>
2025-10-11 17:24:11 -0700

    Added  loopy to Chapter 3

--- start/CH03/Loopy.py
+++ start/CH03/Loopy.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
-# example workign with Loops
-#By 
+# Example working with Loops
+# By Dillon Kierce
+
+# First we have to gather if the day is good or bad, which requires user input
+question = input("Is today a good day? Please answer with y or n: ").lower()
+
+# Analyze the value and print if yes it is
+if question == 'y':
+    # Create a loop of 10 times saying "Yes it is"
+    number = 1
+    while number <= 10:
+        print("Yes it is")
+        number += 1
+elif question == 'n':
+    print("That's okay, tomorrow will be")
+elif question == 'idk':
+    print("That's okay")
+else:
+    print("Please enter a valid input of y or n for yes or no")
