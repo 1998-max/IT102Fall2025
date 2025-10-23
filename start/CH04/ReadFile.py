925479b28dfdbd229f32c174cc3014cc9720bb6a
1998-max <adiyolo.harelboyenge@student.greenriver.edu>
2025-10-22 19:20:50 -0700

    Added Readfile.py in Chapter 4

--- start/CH04/ReadFile.py
+++ start/CH04/ReadFile.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
 # Sample script that reads from a file
-# By 
+# By Harel Boyenge
+# 10/22/2025
+
+# Open file for reading
+hackme_file = open("hackme.txt", "r")
+
+# Read the contents of the file
+content = hackme_file.read()
+
+# Print the required header and the file contents
+print("Here is someone to hack - information")
+print()
+print(content)
+
+# Close the file
+hackme_file.close()
