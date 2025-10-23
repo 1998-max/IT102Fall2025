4b1f0df351ddff14f4d6c091d28ed3ac87b63211
1998-max <adiyolo.harelboyenge@student.greenriver.edu>
2025-10-22 18:59:43 -0700

    Added WriteFIle in Chapter 4

--- start/CH04/WriteFile.py
+++ start/CH04/WriteFile.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
 # Sample script that writes to a file
 # By Harel Boyenge
+# 10/22/2025
 
-# Open the file in write mode
-with open("hackme.txt", "w") as file:
-    name = input("What is your name? ")
-    color = input("What is your favorite color? ")
-    pet = input("What was your first pet's name? ")
-    mother = input("What is your mother's maiden name? ")
-    school = input("What elementary school did you attend? ")
+# Open file for writing
+hackme_file = open("hackme.txt", "w")
 
-    file.write("Name: " + name + "\n")
-    file.write("Favorite Color: " + color + "\n")
-    file.write("First Pet's Name: " + pet + "\n")
-    file.write("Mother's Maiden Name: " + mother + "\n")
-    file.write("Elementary School: " + school + "\n")
+# Write lines to the file
+name = input("What is your name? ")
+color = input("What is your favorite color? ")
+pet = input("What was your first pet's name? ")
+mother = input("What is your mother's maiden name? ")
+school = input("What elementary school did you attend? ")
 
-# Read the file back and print the content
-with open("hackme.txt", "r") as file:
-    content = file.read()
-    print("\nSaved Information:\n")
-    print(content)
+hackme_file.write("Name: " + name + "\n")
+hackme_file.write("Favorite Color: " + color + "\n")
+hackme_file.write("First Pet's Name: " + pet + "\n")
+hackme_file.write("Mother's Maiden Name: " + mother + "\n")
+hackme_file.write("Elementary School: " + school + "\n")
+
+# Close the file
+hackme_file.close()
