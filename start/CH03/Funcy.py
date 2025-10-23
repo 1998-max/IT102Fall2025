4f7604de7719106fb48cf3e17616de0f52e64651
1998-max <adiyolo.harelboyenge@student.greenriver.edu>
2025-10-22 20:13:22 -0700

    Added Funcy.py in Chapter 3

--- start/CH03/Funcy.py
+++ start/CH03/Funcy.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python3
 # example workign with Functions
-#By 
+# By Harel Boyenge
+# 10/22/2025
+
+# Define a function that contains the looping "Yeah it is" message
+def send_message():
+    number = 1
+    while number <= 10:
+        print("Yeah it is")
+        number += 1
+
+# Ask the user for input
+question = input("Is today a good day? Please answer with y or n: ").lower()
+
+# Analyze the input and use the function when appropriate
+if question == 'y':
+    send_message()  
+elif question == 'n':
+    print("That's okay, tomorrow will be")
+elif question == 'idk':
+    print("That's okay")
+else:
+    print("Please enter a valid input of y or n for yes or no")
